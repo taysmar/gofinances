26903f76efb1a939ba34a7e9489d358f479406c1
Taysmar <taysmar@outlook.com>
2021-11-04 23:25:35 -0300

    Alterações na pagina de exibição dos cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from 'react-native'
-
-import { Container, Title } from './styles'
+import { Container, Header, UserWrapper, UserInfo, Photo, User, UserGreeting, UserName, Icon, HighlightCards } from './styles'
+import { RFValue } from "react-native-responsive-fontsize";
+import { HighlightCard } from "../../components/HighlightCard";
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/71301368?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Taysmar</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+            <HighlightCards
+
+            >
+                <HighlightCard type="up" title="Entradas" amount="R$ 17.400,00" lastTransaction="Ultima entrada dia 13 de abril" />
+                <HighlightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Ultima saída dia 03 de abril" />
+                <HighlightCard type="total" title="Total" amount="R$ 16.141,00" lastTransaction="01 à 16 de abril" />
+            </HighlightCards>
         </Container>
     )
 }
